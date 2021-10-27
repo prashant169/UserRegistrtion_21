a37252e9f28083651f05e91c33e6bc5b2d1ffc06
prashant169 <prashantphad2018@gmail.com>
2021-10-26 20:51:42 -0800

    Added: Program for the Ggiven email address is in format or not

--- UserRegistration21/src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ UserRegistration21/src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -5,31 +5,35 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
 
-    UserRegistration userRegistration = new UserRegistration();
+	UserRegistration userRegistration = new UserRegistration();
 
+	@Test
+	public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Prashant");
+		Assert.assertEquals(true, result);
+	}
 
-    @Test
-    public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
-        boolean result = userRegistration.firstName("Prashant");
-        Assert.assertEquals(true, result);
-    }
+	@Test
+	public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
+		boolean result = userRegistration.firstName("prashant");
+		Assert.assertEquals(false, result);
+	}
 
-    @Test
-    public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
-        boolean result = userRegistration.firstName("prashant");
-        Assert.assertEquals(false, result);
-    }
+	@Test
+	public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Phad");
+		Assert.assertEquals(true, result);
+	}
 
-    @Test
-    public void givenLastName_WhenInFormat_ShouldReturnTrue() {
-        boolean result = userRegistration.lastName("Phad");
-        Assert.assertEquals(true, result);
-    }
+	@Test
+	public void givenEmail_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.emailAddress("abc@gmail.com.com");
+		Assert.assertEquals(true, result);
+	}
 
-    @Test
-    public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
-        boolean result = userRegistration.lastName("phad");
-        Assert.assertEquals(false, result);
-
-    }
+	@Test
+	public void givenEmail_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.emailAddress("abc()*@gmail.com");
+		Assert.assertEquals(false, result);
+	}
 }
