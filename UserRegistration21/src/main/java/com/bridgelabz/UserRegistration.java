bfebcade170a86da951ba6ecdd1d459e197667a4
prashant169 <prashantphad2018@gmail.com>
2021-10-26 20:43:10 -0800

    Added: Prog for Last name is in format or not using Test case

--- UserRegistration21/src/main/java/com/bridgelabz/UserRegistration.java
+++ UserRegistration21/src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,9 +5,16 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	public boolean firstName(String firstName) {
-		String regex = "^[A-Z]{1}[a-z]{2}$";
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
+	}
+
+	public boolean lastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
 		return matcher.matches();
 	}
 }
--- UserRegistration21/src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ UserRegistration21/src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -5,17 +5,31 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
 
-	UserRegistration userRegistration = new UserRegistration();
+    UserRegistration userRegistration = new UserRegistration();
 
-	@Test
-	public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
-		boolean result = userRegistration.firstName("Msd");
-		Assert.assertEquals(true, result);
-	}
 
-	@Test
-	public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
-		boolean result = userRegistration.firstName("msd");
-		Assert.assertEquals(false, result);
-	}
+    @Test
+    public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.firstName("Prashant");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
+        boolean result = userRegistration.firstName("prashant");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.lastName("Phad");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
+        boolean result = userRegistration.lastName("phad");
+        Assert.assertEquals(false, result);
+
+    }
 }
