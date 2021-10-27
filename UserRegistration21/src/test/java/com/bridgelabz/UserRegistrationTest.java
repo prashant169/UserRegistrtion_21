71e1817788ad96e8cd9cb32702dabf5e1b749269
prashant169 <prashantphad2018@gmail.com>
2021-10-26 20:31:21 -0800

    Added: Prog the first name is in format or not using Test case

---
+++ UserRegistration21/src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+
+	UserRegistration userRegistration = new UserRegistration();
+
+	@Test
+	public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Msd");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
+		boolean result = userRegistration.firstName("msd");
+		Assert.assertEquals(false, result);
+	}
+}
